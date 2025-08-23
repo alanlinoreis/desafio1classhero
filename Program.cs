8abe044f8376d25a0e54d21f3f9bb36ed5dab4c6
Alan Lino dos Reis <alaanlino@gmail.com>
2025-08-23 20:33:06 -0300

    Criação primeiros arquivos
    Resolução exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//conteúdos de váriaveis string devem ser atribuidas entre "" e não entre ''
+//conteúdos de váriaveis inteiras não podem ter casas decimais;
+//conteúdos de váriaveis char devem ser atribuidas entre '' e não entre ""
+
+string nome = "João";
+int valorProduto = 29;
+char resposta = 'S';
+
+Console.WriteLine(nome);
+Console.WriteLine(valorProduto);
+Console.WriteLine(resposta);
