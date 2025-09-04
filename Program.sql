28a746f95db36eac9c20609599273b04362af1a6
Alan Lino dos Reis <alaanlino@gmail.com>
2025-09-03 21:13:38 -0300

    Update Program.sql

--- Program.sql
+++ Program.sql
@@ -1,24 +1,5 @@
--- 1. Criação da sequence para gerar IDs automaticamente
-CREATE SEQUENCE seq_log_id
-START WITH 1
-INCREMENT BY 1
-NOCACHE
-NOCYCLE;
-
--- 2. Criação da tabela logs
-CREATE TABLE logs (
-    log_id     NUMBER PRIMARY KEY,
-    mensagem   VARCHAR2(255),
-    data_log   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE produtos (
+    produto_id NUMBER PRIMARY KEY,
+    preco      NUMBER CHECK (preco >= 0),
+    status     CHAR(1) CHECK (status IN ('A', 'I'))
 );
-
--- 3. Criação da trigger para preencher log_id automaticamente
-CREATE OR REPLACE TRIGGER trg_logs_bi
-BEFORE INSERT ON logs
-FOR EACH ROW
-BEGIN
-    IF :NEW.log_id IS NULL THEN
-        :NEW.log_id := seq_log_id.NEXTVAL;
-    END IF;
-END;
-/
